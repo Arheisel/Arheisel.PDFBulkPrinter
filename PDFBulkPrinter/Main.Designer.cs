bf2179b4bf58f56ac6ab901ad9f66760a840ff63
Matias Ventura <matias.r.ventura@gmail.com>
2021-05-30 02:21:36 -0300

    Add project files.

---
+++ PDFBulkPrinter/Main.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ArheiselPDFBulkPrinter
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileListBox = new System.Windows.Forms.ListBox();
+            this.currDir = new System.Windows.Forms.TextBox();
+            this.dirButton = new System.Windows.Forms.Button();
+            this.printButton = new System.Windows.Forms.Button();
+            this.printerList = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // fileListBox
+            // 
+            this.fileListBox.FormattingEnabled = true;
+            this.fileListBox.Location = new System.Drawing.Point(13, 39);
+            this.fileListBox.Name = "fileListBox";
+            this.fileListBox.Size = new System.Drawing.Size(347, 316);
+            this.fileListBox.TabIndex = 0;
+            // 
+            // currDir
+            // 
+            this.currDir.Location = new System.Drawing.Point(13, 13);
+            this.currDir.Name = "currDir";
+            this.currDir.ReadOnly = true;
+            this.currDir.Size = new System.Drawing.Size(266, 20);
+            this.currDir.TabIndex = 1;
+            // 
+            // dirButton
+            // 
+            this.dirButton.Location = new System.Drawing.Point(286, 12);
+            this.dirButton.Name = "dirButton";
+            this.dirButton.Size = new System.Drawing.Size(75, 21);
+            this.dirButton.TabIndex = 3;
+            this.dirButton.Text = "Change";
+            this.dirButton.UseVisualStyleBackColor = true;
+            this.dirButton.Click += new System.EventHandler(this.DirButton_Click);
+            // 
+            // printButton
+            // 
+            this.printButton.Enabled = false;
+            this.printButton.Location = new System.Drawing.Point(13, 388);
+            this.printButton.Name = "printButton";
+            this.printButton.Size = new System.Drawing.Size(348, 25);
+            this.printButton.TabIndex = 4;
+            this.printButton.Text = "Print";
+            this.printButton.UseVisualStyleBackColor = true;
+            this.printButton.Click += new System.EventHandler(this.PrintButton_Click);
+            // 
+            // printerList
+            // 
+            this.printerList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.printerList.FormattingEnabled = true;
+            this.printerList.Location = new System.Drawing.Point(13, 361);
+            this.printerList.Name = "printerList";
+            this.printerList.Size = new System.Drawing.Size(347, 21);
+            this.printerList.TabIndex = 5;
+            this.printerList.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Status: Select File";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.printerList);
+            this.Controls.Add(this.printButton);
+            this.Controls.Add(this.dirButton);
+            this.Controls.Add(this.currDir);
+            this.Controls.Add(this.fileListBox);
+            this.Name = "Main";
+            this.Text = "PDF Bulk Printer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox fileListBox;
+        private System.Windows.Forms.TextBox currDir;
+        private System.Windows.Forms.Button dirButton;
+        private System.Windows.Forms.Button printButton;
+        private System.Windows.Forms.ComboBox printerList;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PDFBulkPrinter/Main.cs
@@ -0,0 +1,133 @@
+﻿using PdfiumViewer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArheiselPDFBulkPrinter
+{
+    public partial class Main : Form
+    {
+        List<string> Files = new List<string>();
+
+        string SelectedPath = string.Empty;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            printerList.DataSource = PrinterSettings.InstalledPrinters.Cast<string>().ToList();
+        }
+
+        private void DirButton_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    SelectedPath = fbd.SelectedPath;
+                    currDir.Text = fbd.SelectedPath;
+
+                    Files = Directory.GetFiles(fbd.SelectedPath).ToList();
+
+                    if (Files.Count <= 0) {
+                        printButton.Enabled = false;
+                        fileListBox.Items.Clear();
+                        return;
+                    }
+
+                    var nameList = new List<int>();
+                    foreach(string file in Files)
+                    {
+                        nameList.Add(int.Parse(Path.GetFileNameWithoutExtension(file)));
+                    }
+                    nameList.Sort();
+
+                    fileListBox.Items.Clear();
+                    Files.Clear();
+                    foreach (int fnumber in nameList)
+                    {
+                        Files.Add(Path.Combine(SelectedPath, fnumber + ".pdf"));
+
+                        fileListBox.Items.Add(fnumber + ".pdf");
+                    }
+
+                    printButton.Enabled = true;
+                    label1.Text = "Status: Ready";
+                }
+            }
+        }
+
+        private void PrintButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Create the printer settings for our printer
+                var printerSettings = new PrinterSettings
+                {
+                    PrinterName = (string)printerList.SelectedItem,
+                    Copies = (short)1,
+                };
+
+                // Create our page settings for the paper size selected
+                var pageSettings = new PageSettings(printerSettings)
+                {
+                    Margins = new Margins(0, 0, 0, 0),
+                };
+                foreach (PaperSize paperSize in printerSettings.PaperSizes)
+                {
+                    if (paperSize.PaperName == "A4")
+                    {
+                        pageSettings.PaperSize = paperSize;
+                        break;
+                    }
+                }
+
+                var count = 1;
+                // Now print the PDF document
+                foreach(string file in Files)
+                {
+                    label1.Text = "Status: Printing... " + count + " of " + Files.Count;
+                    using (var document = PdfDocument.Load(file))
+                    {
+                        using (var printDocument = document.CreatePrintDocument())
+                        {
+                            printDocument.PrinterSettings = printerSettings;
+                            printDocument.DefaultPageSettings = pageSettings;
+                            printDocument.PrintController = new StandardPrintController();
+                            printDocument.Print();
+                        }
+                    }
+                    count++;
+                }
+
+                label1.Text = "Status: Done";
+
+                MessageBox.Show("Done Printing!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("ERROR: " + ex.Message, "Exception");
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
